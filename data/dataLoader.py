e705dbddc64af3837e5378e42e276920a98da0e1
akbar <akbar@yantraai.tech>
2020-02-06 12:03:43 +0530

    dataloader code

--- data/dataLoader.py
+++ data/dataLoader.py
@@ -14,13 +14,33 @@ class DataFolder(Dataset):
         self.data_path = os.path.join(self.rootdir, 'data')
         self.label_path = os.path.join(self.rootdir, 'labels')
         self.listdir = os.listdir(self.data_root)
-        if phase
+        if phase == 'train':
+            self.listdir = self.listdir[:int(len(self.listdir)*opt.split)]
+        elif:
+            self.listdir = self.listdir[int(len(self.listdir)*opt.split):]
 
+        opt.dataset_size[phase] = len(self.listdir)
 
+    def __len__(self):
+        # returns length of dataset 
+        return len(self.listdir)
 
+class Options:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+        self.parser.add_argument('--rootdir', default='/home/akbar/data/unet_dataset', help='dataset root directory(where folders having images and labels are there)')
+        self.parser.add_argument('--split', default=0.80, type=float, help='Train/val split ratio')
+        self.parser.add_argument('--batch_size', default=16, type=int, help='train batch size')
+        self.parser.add_argument('--num_workers', default=8, type=int, help='thread count for training')
+        self.parser.add_argument('--shuffle', default=True, help='flag to shuffle the dataset')
+        self.parser.add_argument('--num_epochs', default=100, type=int, help='number of epochs to train')
+        self.parser.add_argument('--image_size', default=512, type=int, help='input image size' )
+        self.opt = self.parser.parse_args()
+        self.opt.dataset_size = {'train':None, 'val':None}
 
-
+    def __call__(self):
+        return self.opt
 
 
 class LoadData:
